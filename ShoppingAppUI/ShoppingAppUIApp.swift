11d2c60b8ce4d520bfe273c13202d8f8a4f9362e
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-10-30 11:06:06 +0530

    Initial Commit

---
+++ ShoppingAppUI/ShoppingAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppUIApp.swift
+//  ShoppingAppUI
+//
+//  Created by Avijeet Pandey on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
